611987c39e4c98982ca96fd08f37b88175086fa2
Inna Mon <inna.mon@gmail.com>
2019-06-23 18:28:02 -0500

    added functionality to add zero only if input is not empty to prevent first value to ever be a zero

--- src/App.js
+++ src/App.js
@@ -20,6 +20,14 @@ class App extends Component {
     this.setState({ input: this.state.input + val });
   }
 
+  addZeroToInput = val => {
+    if (this.state.input !== "") {
+      this.setState({ input: this.state.input + val })
+    }
+  }
+
+  
+
   render() {
     return (
       <div className="App">
@@ -46,8 +54,8 @@ class App extends Component {
             <Button>+</Button>
           </div>
           <div className="row">
-            <Button>.</Button>
-            <Button>0</Button>
+            <Button handleClick={this.addDecimal}>.</Button>
+            <Button handleClick={this.addZeroToInput}>0</Button>
             <Button>=</Button>
             <Button>-</Button>
           </div>
